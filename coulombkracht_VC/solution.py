3d9f1d86a2601f163485236219a61bac4691ca38
ELories <elke.lories@snorduffel.be>
2025-10-07 20:13:08 +0200

    Update solution.py

--- coulombkracht_VC/solution.py
+++ coulombkracht_VC/solution.py
@@ -4,4 +4,4 @@ q2 = 3.0e-6
 r = 5.0e-2  # 0.05 meter
 
 F = k * abs(q1 * q2) / r**2
-print(f"De elektrische kracht is gelijk aan {F:.2f} N")
+print(f"De elektrische kracht is gelijk aan {F:.2f} N.")
