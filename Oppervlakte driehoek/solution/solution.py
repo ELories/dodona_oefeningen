33f741c73425ac6f0372221e1e6d5de4c384a1be
ELories <elke.lories@snorduffel.be>
2025-10-12 20:12:30 +0200

    Create solution.py

---
+++ Oppervlakte driehoek/solution/solution.py
@@ -0,0 +1,12 @@
+# Bereken de omtrek en de oppervlakte van een driehoek
+from math import sqrt
+basis = 5.0
+hoogte = 8.0
+
+schuine_zijde = sqrt(basis**2 + hoogte**2)
+
+omtrek = round(basis + hoogte + schuine_zijde, 1)
+oppervlakte = round((basis * hoogte) / 2, 2)
+
+print("De omtrek van de driehoek is gelijk aan", omtrek, "cm.")
+print("De oppervlakte van de driehoek is gelijk aan", oppervlakte, "cm².")
