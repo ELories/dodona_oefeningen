e6e7ba0dec57d141a272afd19cd7d6bba6c216ad
ELories <elke.lories@snorduffel.be>
2026-01-14 15:30:54 +0100

    Refactor function to accept 'a' as a parameter

--- exponentiële functie 1/solution/solution.py
+++ exponentiële functie 1/solution/solution.py
@@ -1,9 +1,10 @@
 # Definitie van de functie:
-# f(x) = 5 - 4 * (1/3)^(x/2)
-def f(x):
-    y = 5 - 4 * ((1/3) ** (x / 2))
+# f(x) = a - 4 * (1/3)^(x/2)
+def f(a,x):
+    y = a - 4 * ((1/3) ** (x / 2))
     return(round(y,1))
 
+a = float(input("Voer een waarde voor a in: "))
 x = float(input("Voer een waarde voor x in: "))
-y = f(x)
-print("")
+y = f(a,x)
+print("De bijhorende y-waarde is", y,"en de asymptoot ligt op y =",a,".")
